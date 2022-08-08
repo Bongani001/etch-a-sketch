d76da21db4f26f05f24d16244dbb3b63c2bc8456
Bongani001 <bbongs421@gmail.com>
2022-08-08 12:41:22 +0200

    Edit file

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-const container = document.querySelector('.container')
+const container = document.querySelector('.container');
+const userPrompt = document.querySelector('.user-prompt');
 
-for (let i = 0; i < 256; i++) {
-    const grid = document.createElement('div');
-    grid.classList = 'grid';
-    container.appendChild(grid);
+// Create Grids
+function createGrids(size) {
+    for (let i = 0; i < (size * size); i++) {
+        let sides = 500 / size + 'px';
+        const grid = document.createElement('div');
+        grid.classList = 'squareGrid';
+        grid.style.height = sides;
+        grid.style.width = sides;
+        container.appendChild(grid);
+    }
+    changeColor();
 }
 
+// Change grid background color when hovered
+function changeColor() {
+    const grid = document.querySelectorAll('.squareGrid');
+    grid.forEach(square => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'blue';
+        });
+    });
+}
+
+userPrompt.addEventListener('click', () => {
+    const userNumber = prompt('How many number of squares per side would you like?');
+    const grid = document.querySelectorAll('.squareGrid');
+    grid.forEach(square => {
+        square.remove();
+    });
+    createGrids(userNumber);
+})
+
+createGrids(2);
