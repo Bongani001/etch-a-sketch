8b3977746f9b0da559366dc3ecd46348d696c37b
Bongani001 <bbongs421@gmail.com>
2022-08-08 13:18:18 +0200

    Set limit of squares per side to 100

--- script.js
+++ script.js
@@ -25,12 +25,23 @@ function changeColor() {
 }
 
 userPrompt.addEventListener('click', () => {
-    const userNumber = prompt('How many number of squares per side would you like?');
+    let userNumber = prompt('How many number of squares per side would you like? (limit = 100 squares)');
     const grid = document.querySelectorAll('.squareGrid');
     grid.forEach(square => {
         square.remove();
     });
-    createGrids(userNumber);
+    if (isNaN(userNumber)) {
+        Warn = prompt('Please enter a number:');
+        createGrids(Warn); 
+    } else if (userNumber <= 100) {
+        createGrids(userNumber);
+    } else if (userNumber > 100) {
+        Warn = prompt('That is too many squares, limit is 100 squares per side.\n Enter a number that is equal or less than 100:')
+        createGrids(Warn);
+    } else {
+        Warn = prompt('Please enter a number:');
+        createGrids(Warn); 
+    };
 })
 
 createGrids(16);
